5082dee884545b3614de48e5e6335de2b1117278
Wan, Elvis(China - Chengdu) <elviswan@aaxischina.com>
2018-09-13 23:03:55 +0800

    .
    Signed-off-by: Wan, Elvis(China - Chengdu) <elviswan@aaxischina.com>

---
+++ akka-db/src/main/java/com/lightbend/akka/sample/DeviceGroup.java
@@ -0,0 +1,62 @@
+package com.lightbend.akka.sample;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DeviceGroup extends AbstractActor {
+    private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+
+    final String groupId;
+
+    public DeviceGroup(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public static Props props(String groupId) {
+        return Props.create(DeviceGroup.class, groupId);
+    }
+
+    final Map<String, ActorRef> deviceIdToActor = new HashMap<>();
+
+    @Override
+    public void preStart() {
+        log.info("DeviceGroup {} started", groupId);
+    }
+
+    @Override
+    public void postStop() {
+        log.info("DeviceGroup {} stopped", groupId);
+    }
+
+    private void onTrackDevice(DeviceManager.RequestTrackDevice trackMsg) {
+        if (this.groupId.equals(trackMsg.groupId)) {
+            ActorRef deviceActor = deviceIdToActor.get(trackMsg.deviceId);
+            if (deviceActor != null) {
+                deviceActor.forward(trackMsg, getContext());
+            } else {
+                log.info("Creating device actor for {}", trackMsg.deviceId);
+                deviceActor = getContext().actorOf(Device.props(groupId, trackMsg.deviceId), "device-" + trackMsg.deviceId);
+                deviceIdToActor.put(trackMsg.deviceId, deviceActor);
+                deviceActor.forward(trackMsg, getContext());
+            }
+        } else {
+            log.warning(
+                    "Ignoring TrackDevice request for {}. This actor is responsible for {}.",
+                    groupId, this.groupId
+            );
+        }
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(DeviceManager.RequestTrackDevice.class, this::onTrackDevice)
+                .build();
+    }
+}
--- akka-db/src/test/java/com/lightbend/akka/sample/AkkaQuickstartTest.java
+++ akka-db/src/test/java/com/lightbend/akka/sample/AkkaQuickstartTest.java
@@ -1,6 +1,7 @@
 package com.lightbend.akka.sample;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -13,6 +14,8 @@ import com.lightbend.akka.sample.Printer.Greeting;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.testkit.javadsl.TestKit;
+
+import java.util.Optional;
 
 public class AkkaQuickstartTest {
     static ActorSystem system;
@@ -36,5 +39,66 @@ public class AkkaQuickstartTest {
         helloGreeter.tell(new Greet(), ActorRef.noSender());
         Greeting greeting = testProbe.expectMsgClass(Greeting.class);
         assertEquals("Hello, Akka", greeting.message);
+    }
+
+    @Test
+    public void testReplyToRegistrationRequests() {
+        TestKit probe = new TestKit(system);
+        ActorRef deviceActor = system.actorOf(Device.props("group", "device"));
+
+        deviceActor.tell(new DeviceManager.RequestTrackDevice("group", "device"), probe.getRef());
+        probe.expectMsgClass(DeviceManager.DeviceRegistered.class);
+        assertEquals(deviceActor, probe.getLastSender());
+    }
+
+    @Test
+    public void testIgnoreWrongRegistrationRequests() {
+        TestKit probe = new TestKit(system);
+        ActorRef deviceActor = system.actorOf(Device.props("group", "device"));
+
+        deviceActor.tell(new DeviceManager.RequestTrackDevice("wrongGroup", "device"), probe.getRef());
+        probe.expectNoMessage();
+
+        deviceActor.tell(new DeviceManager.RequestTrackDevice("group", "wrongDevice"), probe.getRef());
+        probe.expectNoMessage();
+
+        deviceActor.tell(new Device.RecordTemperature(1,2),probe.getRef());
+        probe.expectMsgClass(Device.TemperatureRecorded.class);
+
+
+        deviceActor.tell(new Device.ReadTemperature(42L), probe.getRef());
+        Device.RespondTemperature response = probe.expectMsgClass(Device.RespondTemperature.class);
+        assertEquals(42L, response.requestId);
+        assertEquals(Optional.of(2.0), response.value);
+    }
+    ///////////////////////////////
+    @Test
+    public void testRegisterDeviceActor() {
+        TestKit probe = new TestKit(system);
+        ActorRef groupActor = system.actorOf(DeviceGroup.props("group"));
+
+        groupActor.tell(new DeviceManager.RequestTrackDevice("group", "device1"), probe.getRef());
+        probe.expectMsgClass(DeviceManager.DeviceRegistered.class);
+        ActorRef deviceActor1 = probe.getLastSender();
+
+        groupActor.tell(new DeviceManager.RequestTrackDevice("group", "device2"), probe.getRef());
+        probe.expectMsgClass(DeviceManager.DeviceRegistered.class);
+        ActorRef deviceActor2 = probe.getLastSender();
+        assertNotEquals(deviceActor1, deviceActor2);
+
+        // Check that the device actors are working
+        deviceActor1.tell(new Device.RecordTemperature(0L, 1.0), probe.getRef());
+        assertEquals(0L, probe.expectMsgClass(Device.TemperatureRecorded.class).requestId);
+        deviceActor2.tell(new Device.RecordTemperature(1L, 2.0), probe.getRef());
+        assertEquals(1L, probe.expectMsgClass(Device.TemperatureRecorded.class).requestId);
+    }
+
+    @Test
+    public void testIgnoreRequestsForWrongGroupId() {
+        TestKit probe = new TestKit(system);
+        ActorRef groupActor = system.actorOf(DeviceGroup.props("group"));
+
+        groupActor.tell(new DeviceManager.RequestTrackDevice("wrongGroup", "device1"), probe.getRef());
+        probe.expectNoMessage();
     }
 }
